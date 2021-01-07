0e97e609365b9a2794493a235629756a419f163d
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-07 15:09:52 -0500

    error resolution.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 require("./routes/apiRoute")(app);
-require("./routes")
+require("./routes/htmlRoute")(app);
 
 app.listen(PORT, function() {
     console.log("App listening on PORT: " + PORT);
