94fbdf6fe9dc9570ce930a6c66f1edcd663c6f19
sziccardi1998 <sziccardifullstack@gmail.com>
2021-01-07 21:23:37 -0500

    proper serving of static elements.

--- server.js
+++ server.js
@@ -7,6 +7,10 @@ var PORT = process.env.PORT || 8080;
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.use(express.static("./public", {
+    extensions: ['html', 'js'],
+}));
+
 require("./routes/apiRoute")(app);
 require("./routes/htmlRoute")(app);
 
